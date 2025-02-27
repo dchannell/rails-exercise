022e18714c98522ed804159237c0c0ecb3f4ec97
Tyler_bowen <tylerbowen@my.unt.edu>
2025-02-27 16:43:11 -0600

    completed the assessment for ApproveShield

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,39 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.order(published_at: :desc)
+    render json: @articles, status: :ok
+  end
+
+  def create
+    begin
+   @article = Article.create!(article_params) 
+   if @article.save
+    render json: @article, status: :created
+   else
+    render json: {errors: @article.errors }, status: :error
+   end
+  rescue Exception => e
+    render json: { errors: e}, status: :bad_request
+  end
+end
+
+  def show
+    @article = Article.find_by_id(params[:id])
+    if @article.present?
+      render json: @article
+    else
+      render  status: :not_found
+    end
+  end
+
+  def not_allowed
+    render status: :method_not_allowed
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :category, :published_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    get "/articles", to: "articles#index"
+    post "/articles", to: "articles#create"
+    get "/article/:id", to: "articles#show"
+    put  "/article/:id", to:"articles#not_allowed" 
+    patch "/article/:id", to:"articles#not_allowed" 
+    delete "/article/:id", to:"articles#not_allowed" 
+
 end
